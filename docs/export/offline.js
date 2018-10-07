f386b8451b9c7738a8a581a462547a5fcce8af28
Fernando Henrique Rosa <ferhrosa@gmail.com>
2018-10-07 00:21:01 -0300

    Add initial prototype export

---
+++ docs/export/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1538882402,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/scroller-sheet0.png",
+		"images/genericobject-sheet0.png",
+		"images/scrollerstoper-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
