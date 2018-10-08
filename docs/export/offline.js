f3a1f096b67713a3c86c6f43ea5c6bb44efd419a
Fernando Henrique Rosa <ferhrosa@gmail.com>
2018-10-07 22:55:55 -0300

    Update published export to current version

--- docs/export/offline.js
+++ docs/export/offline.js
@@ -1,13 +1,16 @@
 ﻿{
-	"version": 1538882402,
+	"version": 1538963724,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
 		"offlineClient.js",
+		"images/enemy_example-sheet0.png",
+		"images/bluebg.png",
+		"images/genericobject-sheet0.png",
+		"images/playerbullet-sheet0.png",
 		"images/player-sheet0.png",
 		"images/scroller-sheet0.png",
-		"images/genericobject-sheet0.png",
 		"images/scrollerstoper-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
